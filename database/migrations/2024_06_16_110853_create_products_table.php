9db048c31ca754d1398ab20a289a6514663d94b7
elangalmajid <almajidelang@gmail.com>
2024-06-16 18:18:25 +0700

    Update

---
+++ database/migrations/2024_06_16_110853_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('merk');
+            $table->string('model');
+            $table->integer('tahun');
+            $table->integer('jarak');
+            $table->string('judul', 100);
+            $table->text('deskripsi');
+            $table->integer('cc');
+            $table->integer('harga');
+            $table->boolean('nego')->default(false);
+            $table->unsignedBigInteger('owner');
+            $table->string('kontak', 15);
+            $table->timestamps();
+
+            $table->foreign('owner')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
